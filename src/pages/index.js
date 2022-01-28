451500cad605c925db412c4dee554e16f56e9ed2
Kettrin62 <Kettykazakova@yandex.ru>
2022-01-28 18:42:45 +0300

    menu mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
+// находим элемент body
+const bodyElement = document.querySelector('.page');
+// находим контейнер с меню
+const menuElement = bodyElement.querySelector('.menu');
+// находим кнопку меню
+const menuButton = bodyElement.querySelector('.button-menu');
+// находим кнопку закрытия меню
+const menuCloseButton = bodyElement.querySelector('.menu__button-close');
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   slidesPerView: 1,
@@ -20,4 +29,24 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper__button-next',
     prevEl: '.swiper__button-prev',
   },
+});
+
+// функция открытия меню
+function openMenu() {
+  menuElement.classList.add('menu_opened');
+}
+
+// функция закрытия меню
+function closeMenu() {
+  menuElement.classList.remove('menu_opened');
+}
+
+// обработчик клика по кнопке меню
+menuButton.addEventListener('click', () => {
+  openMenu();
+});
+
+// обработчик клика по кнопке закрытия меню
+menuCloseButton.addEventListener('click', () => {
+  closeMenu();
 });
